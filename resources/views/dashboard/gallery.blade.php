7cead610913e0b38934672ef52c1283fc89e4334
xturus138 <radityaramadhan.138@gmail.com>
2025-09-16 17:35:58 +0700

    nice

--- resources/views/dashboard/gallery.blade.php
+++ resources/views/dashboard/gallery.blade.php
@@ -638,8 +638,8 @@
             position: fixed;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
+            width: 100vw;
+            height: 100vh;
             background-color: rgba(255, 255, 255, 0.8);
             display: flex;
             justify-content: center;
