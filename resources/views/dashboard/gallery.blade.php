d118d78130ef430371162bc8d30c9609be62f891
xturus138 <radityaramadhan.138@gmail.com>
2025-09-16 13:53:05 +0700

    edit fix

--- resources/views/dashboard/gallery.blade.php
+++ resources/views/dashboard/gallery.blade.php
@@ -22,13 +22,6 @@
                     </svg>
                     Upload
                 </button>
-                <button class="modern-btn secondary-btn" onclick="window.location.href='{{ route('profile.show') }}'">
-                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                        <circle cx="12" cy="7" r="4"></circle>
-                    </svg>
-                    Profile
-                </button>
             </div>
         </div>
         <div class="storage-progress">
